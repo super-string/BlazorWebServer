2a40f32016724312668933fa55f12820a6a17466
super-string <sh5937@gmail.com>
2023-01-03 13:46:12 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorWebServer/Server/Controllers/GitHubController.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebServer.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebServer.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<GitHub> Get()
+        {
+            return new List<GitHub>()
+            {
+                new GitHub(){Name = "hoge"},
+                new GitHub(){Name = "piyo"},
+            }.ToArray();
+        }
+    }
+}
